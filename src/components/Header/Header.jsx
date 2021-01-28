8e3014a32214208fbc0018b6dfcee22ac6cd7ad5
Esteban Monguí <esteband.itorres@gmail.com>
2021-01-27 21:38:01 -0500

    fix: header return

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import "./Header.scss";
 //Components
-import ExitButton from "../Buttons/ExitButton/ExitButton.jsx"
+import ExitButton from "../Buttons/ExitButton/ExitButton.jsx";
 
-const Header=()=>{
-    return(
-        <div className="Header">
-            <h3 className="__Logo">My<span>Bank</span></h3>
-            <nav>
-                <ul className="__Items">
-                    <li><a href="">Credits</a></li>
-                    <li><a href="">About us</a></li>
-                    <li><a href="">Laws</a></li>
-                </ul>
-            </nav>
-            <ExitButton ButtonText="exit"/>
-        </div>
-    )
-}
+const Header= () => (
+    <div className="Header">
+        <h3 className="__Logo">My<span>Bank</span></h3>
+        <nav>
+            <ul className="__Items">
+                <li><a href="">Credits</a></li>
+                <li><a href="">About us</a></li>
+                <li><a href="">Laws</a></li>
+            </ul>
+        </nav>
+        <ExitButton ButtonText="exit" />
+    </div>
+)
 
 export default Header;
