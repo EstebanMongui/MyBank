7a445baa24081314b5d91471a0abd42e4449ed1a
Esteban Monguí <esteband.itorres@gmail.com>
2021-01-28 10:02:11 -0500

    feat: Home component V1.0

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import './Home.scss'
 //Components
+import Header from "../../components/Header/Header.jsx"
 import Card from "../../components/Card/Card.jsx"
+import TransactionButton from "../../components/Buttons/TransactionButton/TransactionButton.jsx"
 
 class Home extends React.Component {
     render() {
+        const{
+            title="MyBank transactions and stay save",
+            description="Make your transactions now is more cheap and more easy. What do you wait for make that?"
+        }=this.props
         return(
-            <div className='Home'>
-            </div>
+            <section className='Home'>
+                <div className="Home__Container">
+                    <Header />
+                    <div className="Home__TextSection">
+                        <h1>{title}</h1>
+                        <h5>{description}</h5>
+                        <TransactionButton/>
+                    </div>
+                    <Card />
+                </div>
+            </section>
         )
     }
 };
