72146ccc9d482c9d34d2d082b5fd988c3ae583fd
Esteban Monguí <esteband.itorres@gmail.com>
2021-01-27 21:22:21 -0500

    feat: header V1.0

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Header.scss";
+//Components
+import ExitButton from "../Buttons/ExitButton/ExitButton.jsx"
+
+const Header=()=>{
+    return(
+        <div className="Header">
+            <h3 className="__Logo">My<span>Bank</span></h3>
+            <nav>
+                <ul className="__Items">
+                    <li><a href="">Credits</a></li>
+                    <li><a href="">About us</a></li>
+                    <li><a href="">Laws</a></li>
+                </ul>
+            </nav>
+            <ExitButton ButtonText="exit"/>
+        </div>
+    )
+}
+
+export default Header;
