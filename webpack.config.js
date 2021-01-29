444ab8fd44ff5e236ef3eb3889cf4e0855dac2c4
Esteban Monguí <esteband.itorres@gmail.com>
2021-01-28 21:29:34 -0500

    Deploy V1.0

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPligin = require('mini-css-extract-plugin');
 module.exports = {
+    mode:'production',
     entry:'./src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
